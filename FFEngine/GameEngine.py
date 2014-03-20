70dcda513e80de1669c98c4fecb57f127d06b73f
Tim Barton <tim@vouchercloud.com>
2014-03-20 15:43:20 +0000

    Enhanced the combat a little

--- FFEngine/GameEngine.py
+++ FFEngine/GameEngine.py
@@ -2,7 +2,12 @@ import GameParser
 import random
 
 class GameRunner(object):
+    def __init__(self):
+        self.Game = None
+
     def Run(self, game):
+        print("Skill: {0} Stamina: {1} Luck: {2}".format(game.Stats.Skill, game.Stats.Stamina, game.Stats.Luck))
+        self.Game = game
         self.ProcessPage(self.GetPage(game.Data.pages, "1"))
 
     def GetPage(self, pageCollection, page):
@@ -22,15 +27,22 @@ class GameRunner(object):
     def ProcessFight(self, combats):
         for combat in combats:
             print("{0}: skill {1}: stamina {2}".format(combat.name, combat.skill, combat.stamina))
-            #result = raw_input("Attack:")
-            yourRoll = self.GetCombatRollValue()
-            theirRoll = self.GetCombatRollValue()
+            result = input("Attack:")
+            yourRoll = self.GetCombatRollValue(self.Game.Stats.Skill)
+            theirRoll = self.GetCombatRollValue(combat.skill)
             print("outcome: you {0}/{1} {2}".format(yourRoll, combat.name, theirRoll))
 
-
-    def GetCombatRollValue(self):
-        return random.randint(1, 6) + random.randint(1, 6)
+    def GetCombatRollValue(self, skill):
+        return skill + random.randint(1, 6)
 
 class Game():
     def __init__(self, game):
         self.Data = GameParser.json2obj(game)
+        self.Stats = Stats()
+
+class Stats():
+    def __init__(self):
+        self.Skill = random.randint(1, 6) + 6
+        self.Luck = random.randint(1, 6) + 6
+        self.Stamina = random.randint(1, 6) + 12
+
